84f8961491862d660b7cedebb89148087d2c250f
Christoffer Sladin <102432146+ChristofferSladin@users.noreply.github.com>
2024-12-13 13:15:55 +0100

    Update streamlit_app.py
    fixed error caused in loop

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,18 +5,15 @@ from snowflake.snowpark.functions import col
 
 # Write directly to the app
 st.title("My Parents New Healthy Diner")
-st.write(
-    "Choose the fruits you want in your custom Smoothie!"
-)
+st.write("Choose the fruits you want in your custom Smoothie!")
 
 name_on_order = st.text_input("Name on smoothie:")
 st.write("The name on your smoothie will be:", name_on_order)
 
 cnx = st.connection("snowflake")
 session = cnx.session()
-my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
-#st.dataframe(data=my_dataframe, use_container_width=True)
 
+my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
 ingredients_list = st.multiselect(
     'Choose up to 5 ingridients:'
     , my_dataframe
@@ -25,29 +22,18 @@ ingredients_list = st.multiselect(
 
 if ingredients_list:
     ingredients_string = ''
-    
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
         st.subheader(fruit_chosen + ' Nutrition Information')
         smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-        sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
+        response_json = smoothiefroot_response.json()
+        st.dataframe(response_json, use_container_width=True)
 
     st.dataframe(sf_df)
-
-    
     my_insert_stmt = """ insert into smoothies.public.orders(ingredients, name_on_order)
             values ('""" + ingredients_string + """','"""+name_on_order+"""')"""
-
-    
-    #st.write(my_insert_stmt)
-    #st.stop
-    
-    
     time_to_insert = st.button('Submit Order')
 
     if time_to_insert:
         session.sql(my_insert_stmt).collect()
         st.success('Your Smoothie is ordered!', icon="✅")
-
-
-
